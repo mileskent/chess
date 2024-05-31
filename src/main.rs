89ca3de5c56ba22a5beccd9d0351c925a11b499d
AlexKCA <mileskent13@gmail.com>
2024-05-31 11:06:31 -0700

    Basic Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn conf() -> Conf {
+    let size = 800;
+    Conf {
+        window_title: "Chess".to_string(), //this field is not optional!
+        fullscreen:false,
+        window_width:size,
+        window_height:size,
+        //you can add other options too, or just use the default ones:
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(conf)]
+async fn main() {
+    loop {
+        clear_background(MAGENTA);
+
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        next_frame().await
+    } 
 }

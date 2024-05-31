30deb5253b193e3774e8ff0511a69e7b4c398bce
AlexKCA <mileskent13@gmail.com>
2024-05-31 16:33:25 -0700

    Dragging works now

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use std::process;
 const SIDE_LENGTH : u32 = 8;
 const S: u32 = 100;
 
-macro_rules! XW {
+macro_rules! SQUARE_WIDTH {
     () => {
         (screen_width() / SIDE_LENGTH as f32) as u32
     };
 }
 
-macro_rules! YW {
+macro_rules! SQUARE_HEIGHT {
     () => {
         (screen_height() / SIDE_LENGTH as f32) as u32
     };
@@ -25,8 +25,8 @@ fn m2xy() -> (u32, u32) {
 }
 
 fn i2xy(i: u32) -> (u32, u32) {
-   let x = (i % SIDE_LENGTH) * XW!();
-   let y = i / SIDE_LENGTH * YW!();
+   let x = (i % SIDE_LENGTH) * SQUARE_WIDTH!();
+   let y = i / SIDE_LENGTH * SQUARE_HEIGHT!();
    (x, y)
 }
 
@@ -48,27 +48,34 @@ fn conf() -> Conf {
 
 fn draw_pieces(piece_textures: HashMap<char, Texture2D>, pieces: Vec<char>) {
     let mut boardi = 0;
-    for c in pieces {
-        if c != 'X' {
-            if let Some(texture) = piece_textures.get(&c) {
-                let (x, y) = i2xy(boardi);
-                // println!("Putting {c} on i: {boardi}, x: {x}, y: {y}");
-                draw_texture_ex(
-                    &texture,
-                    x as f32,
-                    y as f32,
-                    WHITE,
-                    DrawTextureParams {
-                        dest_size: Some(Vec2::new(XW!() as f32, YW!() as f32)),
-                        ..Default::default()
-                    },
-                );
-            }
-            else {
-                break;
-            }
+    for c in &pieces {
+        if *c != 'X' {
+            let (x, y) = i2xy(boardi);
+            draw_from_char(piece_textures.clone(), pieces.clone(), *c, x as f32, y as f32);
         }
         boardi += 1;
+    }
+}
+
+fn draw_from_char (piece_textures: HashMap<char, Texture2D>, pieces: Vec<char>, c: char, x: f32, y: f32) {
+    if c != 'X' {
+        if let Some(texture) = piece_textures.get(&c) {
+            // println!("Putting {c} on i: {boardi}, x: {x}, y: {y}");
+            draw_texture_ex(
+                &texture,
+                x,
+                y,
+                WHITE,
+                DrawTextureParams {
+                    dest_size: Some(Vec2::new(SQUARE_WIDTH!() as f32, SQUARE_HEIGHT!() as f32)),
+                    ..Default::default()
+                },
+            );
+        }
+        else {
+            eprintln!("ERROR! Texture not found. c: {:?}", c);
+            process::exit(1);
+        }
     }
 }
 
@@ -92,7 +99,7 @@ fn draw_pieces(piece_textures: HashMap<char, Texture2D>, pieces: Vec<char>) {
 //                     y as f32,
 //                     WHITE,
 //                     DrawTextureParams {
-//                         dest_size: Some(Vec2::new(XW!() as f32, YW!() as f32)),
+//                         dest_size: Some(Vec2::new(SQUARE_WIDTH!() as f32, SQUARE_HEIGHT!() as f32)),
 //                         ..Default::default()
 //                     },
 //                 );
@@ -109,10 +116,10 @@ fn draw_board() {
     for x in 0..SIDE_LENGTH {
         for y in 0..SIDE_LENGTH {
             draw_rectangle(
-                (x * XW!()) as f32,
-                (y * YW!()) as f32,
-                XW!() as f32,
-                YW!() as f32,
+                (x * SQUARE_WIDTH!()) as f32,
+                (y * SQUARE_HEIGHT!()) as f32,
+                SQUARE_WIDTH!() as f32,
+                SQUARE_HEIGHT!() as f32,
                 if (x + y) % 2 == 0 {WHITE} else {BROWN});
         }
     }
@@ -168,23 +175,42 @@ async fn main() {
             c = Color::new(0.,0.,0.,0.1)
         }
 
-        if dragging == true && is_mouse_button_released(MouseButton::Left) {
-            dragging = false;
-            if let Some(value) = last_selected {
-                pieces[value as usize] = last_piece;
+        if dragging == true {
+
+            if is_mouse_button_released(MouseButton::Left) {
+                dragging = false;
+                let legal: bool = true;
+                let (a,b) = m2xy(); 
+                let now_selected = xy2i(a,b) as u32; 
+                if let Some(value) = last_selected {
+                    // if illegel or you didn't move the piece
+                    if !legal || now_selected == value {
+                        pieces[value as usize] = last_piece;
+                    } 
+                    // if legal (move the piece)
+                    else {
+                        pieces[now_selected as usize] = last_piece;
+                    }
+                }
+                else {
+                    eprintln!("last_selected is not yet initialized but is being accessed");
+                    process::exit(1);
+                }
             }
             else {
-                eprintln!("last_selected is not yet initialized but is being accessed");
-                process::exit(1);
+                // Draw the piece on the mouse
+                let (x, y) = mouse_position();
+                draw_from_char(piece_textures.clone(), pieces.clone(), last_piece, x - (SQUARE_WIDTH!() / 2) as f32, y - (SQUARE_HEIGHT!() / 2) as f32);
             }
         }
-
+        
+        // Hilighting
         let (x,y) = m2xy();
         draw_rectangle(
-                (x * XW!()) as f32,
-                (y * YW!()) as f32,
-                XW!() as f32,
-                YW!() as f32,
+                (x * SQUARE_WIDTH!()) as f32,
+                (y * SQUARE_HEIGHT!()) as f32,
+                SQUARE_WIDTH!() as f32,
+                SQUARE_HEIGHT!() as f32,
                 c 
                 );
         
